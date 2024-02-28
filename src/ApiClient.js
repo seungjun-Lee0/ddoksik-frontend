4af2a7ad7d658d4e6de7bbd9b1c0ed843ade5eaf
Sejun Yun <bangwol08@naver.com>
2024-02-28 00:30:18 +0000

    api url change

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // API 기본 URL 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://www.bangwol08.com';
 
 // 공통 axios 인스턴스 생성
 const apiClient = axios.create({
