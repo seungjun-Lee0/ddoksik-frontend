f6a23e0ed2c227ba5f8aad1a54aa80b61e1514bb
Sejun Yun <bangwol08@naver.com>
2024-03-04 08:22:54 +0000

    Update Api host

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // API 기본 URL 
-const API_BASE_URL = 'http://www.bangwol08.com';
+const API_BASE_URL = 'https://www.ddoksik2.site';
 
 // 공통 axios 인스턴스 생성
 const apiClient = axios.create({
